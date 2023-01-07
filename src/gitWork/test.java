49cc3ea173e9b64a3780e4f8b06272b2d6a32295
soukaina <soukaina.elmir.93@gmail.com>
2023-01-06 21:35:21 -0700

    creat test file

---
+++ src/gitWork/test.java
@@ -0,0 +1,15 @@
+package gitWork;
+
+public class test {
+    public static void main(String[] args) {
+
+        System.out.println("grocery");
+        System.out.println("neil is crying ");
+        System.out.println("when we gonna get an offer inshaelah");
+
+
+
+
+
+    }
+}
